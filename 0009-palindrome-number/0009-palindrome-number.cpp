f41d5a594c952ba6e93b4ec8725f868e227f7c13
Saksham Pandey <salvatoredamom97@gmail.com>
2026-07-10 18:28:29 +0530

    Time: 2 ms (50.89%), Space: 8.6 MB (38.09%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long y=x;
+        long rev = 0;
+        while (x>0)
+        {
+            rev=rev*10 + x%10;
+            x/=10;
+        }
+        if (rev==y) return true;
+        else return false;
+    }
+};
