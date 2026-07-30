e5bf15190331f9872e134d9ca290f875c3eecbae
Saksham Pandey <salvatoredamom97@gmail.com>
2026-07-30 16:02:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int sum=0;;
+        for (int i=0 ; i < word.size() ; i++)
+        sum+= i/8 +1;
+        return sum;
+    }
+};
