438d67354430f4eb462dbb2c3a41eb22b001db7d
Saksham Pandey <salvatoredamom97@gmail.com>
2026-08-06 19:46:02 +0530

    Time: 8 ms (96.49%), Space: 45.9 MB (5.54%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& nums, int limit) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        int count=0;
+        while (i <= j)
+        {
+            if (nums[i] + nums[j]   <= limit)
+            {
+                count ++;
+                i++;
+                j--;
+            }
+            else if (nums[j] == limit)
+            {
+                count++;
+                j--;
+            }
+            else if (nums[i] + nums[j] > limit)
+            {
+                count ++;
+                j--;
+            }
+        }
+        return count;
+    }
+};
