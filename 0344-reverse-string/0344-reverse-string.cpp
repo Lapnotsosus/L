d181d3bf9fa5a89c4f7a0386835fae3a9aed2906
Saksham Pandey <salvatoredamom97@gmail.com>
2026-08-29 16:14:50 +0530

    Time: 0 ms (100%), Space: 27.3 MB (52.42%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void solve(vector<char>&s , int l , int r)
+    {
+        if (l>=r)
+        return;
+        
+        swap(s[l],s[r]);
+        solve(s,l+1,r-1);
+    }
+
+    void reverseString(vector<char>& s) {
+        solve(s , 0 , s.size()-1);
+    }
+};
